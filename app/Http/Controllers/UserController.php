69019119b389704b949922cf3c083d9098674359
Teddy Pascual <teddylayson2000@gmail.com>
2024-10-30 16:38:38 +0800

    add pagination

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -63,7 +63,7 @@ class UserController extends Controller
     }
     public function showCorrectHomepage() {
         if (auth()->check()) {
-           return view('homepage-feed', ['posts' => auth()->user()->feedPosts()->latest()->get()]);
+           return view('homepage-feed', ['posts' => auth()->user()->feedPosts()->latest()->paginate(2)]);
         } else {
             return view('homepage');
         }
